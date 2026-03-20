91136a74b4b9517df6d627365bc9df0517f5d743
La-Kiks <kilian.audroin@gmail.com>
2026-03-20 12:27:43 +0100

    MongoDB setup with docker compose + small fixes

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,18 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("Connection error", err));
 
 module.exports = app;
